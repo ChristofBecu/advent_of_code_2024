b11c393518598195ebcd8b8b002336ad49e61dda
Christof Becu <christof.becu@gmail.com>
2024-12-01 10:52:04 +0100

    Add solution and description for Day 1 puzzle part 2

---
+++ day_01/part2.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main() {
+  std::vector<int> leftNumbers;
+  std::vector<int> rightNumbers;
+
+  std::fstream file("input.txt");
+
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::istringstream stream(buffer.str());
+  int left, right;
+
+  while (stream >> left >> right) {
+    leftNumbers.push_back(left);
+    rightNumbers.push_back(right);
+  }
+
+  int simmilarity = 0;
+  std::vector<int> simmilarities;
+
+  for (int i = 0; i < leftNumbers.size(); i++) {
+    int leftNumber = leftNumbers[i];
+
+    int count =
+        std::count(rightNumbers.begin(), rightNumbers.end(), leftNumber);
+
+    simmilarity = leftNumber * count;
+
+    simmilarities.push_back(simmilarity);
+  }
+
+  long sum = 0;
+  for (int i = 0; i < simmilarities.size(); i++) {
+    sum += simmilarities[i];
+  }
+
+  std::cout << sum << std::endl; // 22545250
+
+  return 0;
+}
